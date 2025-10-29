a1eb93add8c77b787e459fe8791d398a66757e7e
akshay <akshaybachhu16@gmail.com>
2025-10-29 18:54:11 +0000

    Deployed Contract Using a Script File

---
+++ script/DeploySimpleStorage.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import "forge-std/Script.sol";
+import "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast();
+        // we can also send value like this
+        // SimpleStorage simpleStorage = new SimpleStorage{value: 1 ether}();
+        SimpleStorage simpleStorage = new SimpleStorage(); // the simpleStorage stores the address of the deployed
+        // contract
+        vm.stopBroadcast();
+        return simpleStorage;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
